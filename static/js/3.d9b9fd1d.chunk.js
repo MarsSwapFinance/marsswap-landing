7129463a0d56033a5c420f75cdbbc7754df20525
MarsSwapFinance <marsswap@tutanota.com>
2021-04-05 18:48:51 +0200

    Updates

---
+++ static/js/3.d9b9fd1d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmarsswap-landing"]=this["webpackJsonpmarsswap-landing"]||[]).push([[3],{115:function(s,t,a){"use strict";a.r(t);var i=a(1);t.default=function(s){var t=s.src,a=s.width,n=s.height;return Object(i.jsx)("img",{src:"/img/svg/".concat(t),alt:t,with:a,height:n})}}}]);
+//# sourceMappingURL=3.d9b9fd1d.chunk.js.map
